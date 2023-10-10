65a3ef3a97d1a3b57c31c3126d4029a33fdc472d
ub691 <x9012195407x@gmail.com>
2023-10-10 10:08:03 +0800

    test3

--- 2.21/source/main.c
+++ 2.21/source/main.c
@@ -7,7 +7,7 @@ int main()
 	printf("*       *         *     *         ***            * *\n");
 	printf("*       *        *       * ¡@¡@¡@*****          *   *\n");
 	printf("*       *        *       *         *           *     *\n");
-	printf("*       *        *       *¡@¡@¡@¡@ *          *       *\n");
+	printf("*       *        *       *         *          *       *\n");
 	printf("*       *        *       *         *           *     *\n");
 	printf("*       *        *       *         *            *   *\n");
 	printf("*       *         *     *          *             * *\n");
